1e0d7903a2e144bba96a8c1f1436d24b63aea977
renzhenqi <qinze5577@gmail.com>
2023-07-29 23:48:17 +0800

    init

---
+++ a.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: renzhenqi qinze5577@gmail.com
+ * @Date: 2023-07-28 07:37:46
+ * @LastEditors: renzhenqi qinze5577@gmail.com
+ * @LastEditTime: 2023-07-28 07:40:25
+ * @FilePath: /nextjs-blog/a.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import b from './b.js'
+
+console.warn(b)
---
+++ b.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: renzhenqi qinze5577@gmail.com
+ * @Date: 2023-07-28 07:37:51
+ * @LastEditors: renzhenqi qinze5577@gmail.com
+ * @LastEditTime: 2023-07-28 07:45:29
+ * @FilePath: /nextjs-blog/b.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export const b = 'hello'
+
+const newB = 'new'
+
+export default newB
